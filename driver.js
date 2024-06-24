dba3f3561b063cff8bcd518972f2da57664c5168
Charliecoleg <105607648+Charliecoleg@users.noreply.github.com>
2024-06-24 14:39:35 +0100

    Delete driver.js

--- driver.js
+++
@@ -1,4 +0,0 @@
-// driver.js
-const htmlContent = getInput("htmlContent"); // Get input from Glide
-const dataUrl = convertHtmlToDataUrl(htmlContent); // Call your JavaScript function
-setResult("dataUrl", dataUrl); // Set the result back to Glide
